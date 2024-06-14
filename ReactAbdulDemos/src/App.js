2c1473390d2343f8576e02c41261658aab8fe492
Yesdani <yasdanis@gmail.com>
2024-06-14 11:33:27 +0530

    Add files via upload

---
+++ ReactAbdulDemos/src/App.js
@@ -0,0 +1,12 @@
+import BtnEvents from "./BtnEvents";
+import Gallery from "./StateUsest";
+import TaskApp from "./Tasks";
+
+export default function App(){
+  return(
+        <>
+        <TaskApp />
+        <BtnEvents />
+        </>
+       );
+}
